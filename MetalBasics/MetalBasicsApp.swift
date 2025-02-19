f0d49b2f72ab1815ed6d4d0b902eca8d38049cb9
Mathieu Dubart <mathieu.dubart@icloud.com>
2025-02-19 13:32:00 +0100

    Initial Commit

---
+++ MetalBasics/MetalBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalBasicsApp.swift
+//  MetalBasics
+//
+//  Created by Mathieu Dubart on 19/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct MetalBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
